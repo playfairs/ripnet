c2519ed68e556b53ee7b0f2ad5a423b2ea56d6f8
playfairs <74739795+playfairs@users.noreply.github.com>
2026-09-06 09:06:55 -0500

    nix: fix flake

--- flake.nix
+++ flake.nix
@@ -3,23 +3,50 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs }:
+  outputs = { self, nixpkgs, treefmt-nix }:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      formatterFor = system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+          import ./nix/formatter.nix {
+            inherit pkgs self treefmt-nix;
+          };
     in {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          gcc
-          gdb
-          valgrind
-          meson
-          ninja
-          pkg-config
-          libpcap
-        ];
-      };
+      devShells = nixpkgs.lib.genAttrs systems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              gcc
+              gdb
+              meson
+              ninja
+              pkg-config
+              libpcap
+            ] ++ pkgs.lib.optionals pkgs.stdenv.hostPlatform.isLinux [
+              valgrind
+            ];
+          };
+        });
+
+      formatter = nixpkgs.lib.genAttrs systems (system:
+        (formatterFor system).wrapper);
+
+      checks = nixpkgs.lib.genAttrs systems (system: {
+        formatting = (formatterFor system).check;
+      });
     };
 }
