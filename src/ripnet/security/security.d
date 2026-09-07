ab43d4fdebb280a96387e65177d45898ed941d95
playfairs <74739795+playfairs@users.noreply.github.com>
2026-09-07 06:44:08 -0500

    refactor(security): isolate SSH transports from probes

--- src/ripnet/security/security.d
+++ src/ripnet/security/security.d
@@ -2,42 +2,6 @@ module ripnet.security.security;
 
 import ripnet.model;
 import ripnet.network.probe : tcpProbe;
-import std.process : Pid, spawnProcess, wait;
-import std.stdio : stderr, stdin, stdout;
-import std.conv : to;
-
-public PingResult ssh(string host, ushort port = 22)
-{
-    return tcpProbe(host, port);
-}
-
-public int connectSsh(
-    string host,
-    string user,
-    ushort port,
-    string identity,
-    string remoteCommand,
-)
-{
-    string[] command = ["ssh"];
-    command ~= ["-p", to!string(port)];
-    if (identity.length)
-        command ~= ["-i", identity];
-    command ~= user.length ? user ~ "@" ~ host : host;
-    if (remoteCommand.length)
-        command ~= [remoteCommand];
-
-    try
-    {
-        Pid process = spawnProcess(command, stdin, stdout, stderr);
-        return wait(process);
-    }
-    catch (Exception exception)
-    {
-        stderr.writeln("ripnet: unable to start ssh: ", exception.msg);
-        return 127;
-    }
-}
 
 public PingResult http(string host, ushort port = 80)
 {
